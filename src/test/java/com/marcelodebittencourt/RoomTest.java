02126539b2888075f690864eaea2518da7f5c255
GabrielDelazeri <321gabrielz@gmail.com>
2021-11-06 17:41:59 -0300

    Teste

--- src/test/java/com/marcelodebittencourt/RoomTest.java
+++ src/test/java/com/marcelodebittencourt/RoomTest.java
@@ -2,17 +2,20 @@ package com.marcelodebittencourt;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class RoomTest {
+
     @Test
     //Teste novo quarto
     void testNewRoom(){
         //Entrada de Dados - Massa
         int roomnumber = 200;
         String RoomName = "Suíte Luxo";
-        Double PriceDay = 500;
+        double PriceDay = 500;
 
         //T - Teste - Execução teste
-        Room luxureroom = New Room(roomnumber, RoomName, PriceDay);
+        Room luxureroom = new Room(roomnumber, RoomName, PriceDay);
 
         //R - Resultado - Validação de dados
         assertEquals(roomnumber, luxureroom.getId());
